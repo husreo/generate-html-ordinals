04272d683db9d1bf69ab3475b39f1d3aea58eb2f
husreo <springers230@gmail.com>
2024-08-13 20:51:03 -0700

    update:metadata

--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
 import fs from 'fs';
-
+const Race = ["FISHMAN", "Lunarian"];
+const traits = ["Extra", "HairFront", "Clothes", "Mouth", "Eyes", "Skins", "HairBack", "Back"]
 const metadata = JSON.parse(fs.readFileSync('metadata.json', 'utf-8'));
 type Metadata = {name: string; description:string;}[]
 type TraitsMetadataType = Metadata[];
 for (let index = 0; index <= 1; index++) {
-    const charactor = metadata.filter((val: {name: string, description: string}) => Number(val.name.split(',')[3]) === index)
+    const charactor = metadata.filter((val: {name: string, description: string}) => val.name.split('/')[1] === Race[index])
     let array_traits: TraitsMetadataType = [];
     
     for (let j = 0; j < 8; j++) {
-        const element = charactor.filter((val: {name: string, descriptin: string}) => Number(val.name.split(',')[4]) === j);
+        const element = charactor.filter((val: {name: string, descriptin: string}) => val.name.split('/')[2] === traits[j]);
         array_traits.push(element);
     }
-    // for (let j = 0; j < 8; j++) {
-        
-    // }
     console.log(`charactor ${index} \'s tratis:`, array_traits);
 }
 
