7f20420b9207d7c0f28f57c50b3b203d98361121
husreo <springers230@gmail.com>
2024-08-13 18:07:59 -0700

    update: global valuable.

--- index.ts
+++ index.ts
@@ -3,17 +3,17 @@ import fs from 'fs';
 const metadata = JSON.parse(fs.readFileSync('metadata.json', 'utf-8'));
 type Metadata = {name: string; description:string;}[]
 type TraitsMetadataType = Metadata[];
-let array_traits: TraitsMetadataType = [];
 for (let index = 0; index <= 1; index++) {
     const charactor = metadata.filter((val: {name: string, description: string}) => Number(val.name.split(',')[3]) === index)
+    let array_traits: TraitsMetadataType = [];
     
     for (let j = 0; j < 8; j++) {
         const element = charactor.filter((val: {name: string, descriptin: string}) => Number(val.name.split(',')[4]) === j);
         array_traits.push(element);
     }
-    for (let j = 0; j < 8; j++) {
+    // for (let j = 0; j < 8; j++) {
         
-    }
+    // }
     console.log(`charactor ${index} \'s tratis:`, array_traits);
 }
 
